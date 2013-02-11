c901267b8d0519e65f86b29ff88929c2d35a4316
Ben Johnson <bkbjohnson@gmail.com>
2013-02-11 18:58:14 -0500

    Terrible early version that has title-gameplay-title flow

--- Crystallography/Crystallography/AppMain.cs
+++ Crystallography/Crystallography/AppMain.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 
 using Sce.PlayStation.Core;
 using Sce.PlayStation.Core.Environment;
-using Sce.PlayStation.Core.Graphics;
+//using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
 
-// HI PHOENIX!
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
 namespace Crystallography
 {
 	public class AppMain
 	{
-		private static GraphicsContext graphics;
+		//private static GraphicsContext graphics;
 		
 		public static void Main (string[] args)
 		{
@@ -27,24 +28,30 @@ namespace Crystallography
 
 		public static void Initialize ()
 		{
-			// Set up the graphics system
-			graphics = new GraphicsContext ();
+			Director.Initialize();
+			Game.Instance = new Game();
+			Game.Instance.Initialize();
+			
+			Vector2 ideal_screen_size = new Vector2(960.0f, 544.0f);
+			Camera2D camera = Game.Instance.GameScene.Camera as Camera2D;
+			camera.SetViewFromHeightAndCenter(ideal_screen_size.Y, ideal_screen_size / 2.0f);
 		}
 
 		public static void Update ()
 		{
 			// Query gamepad for current state
 			var gamePadData = GamePad.GetData (0);
+			
+			Director.Instance.Update();
 		}
 
 		public static void Render ()
 		{
-			// Clear the screen
-			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-			graphics.Clear ();
+			Director.Instance.Render();
 
 			// Present the screen
-			graphics.SwapBuffers ();
+			Sce.PlayStation.HighLevel.GameEngine2D.Director.Instance.GL.Context.SwapBuffers();
+			Director.Instance.PostSwap();
 		}
 	}
 }
--- Crystallography/Crystallography/NoCleanupScene.cs
+++ Crystallography/Crystallography/NoCleanupScene.cs
@@ -1,1 +1,25 @@
+/* SCE CONFIDENTIAL
+ * PlayStation(R)Suite SDK 0.98.2
+ * Copyright (C) 2013 Sony Computer Entertainment Inc.
+ * All Rights Reserved.
+ */
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+
+namespace Crystallography
+{
+	public class NoCleanupScene : Scene
+	{
+		public override void OnEnter()
+		{
+			base.OnEnter();
+		}
+
+		public override void OnExit()
+		{
+			StopAllActions();
+		}
+	}
+
+}
 
