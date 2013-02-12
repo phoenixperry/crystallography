8a3cce5a5d5f11ae3e3be89c9fdee3da36862ac7
Ben Johnson <bkbjohnson@gmail.com>
2013-02-11 22:23:45 -0500

    Reimplementing menu stuff

--- Crystallography/Crystallography/AppMain.cs
+++ Crystallography/Crystallography/AppMain.cs
@@ -1,57 +1,20 @@
 using System;
-using System.Collections.Generic;
 
-using Sce.PlayStation.Core;
-using Sce.PlayStation.Core.Environment;
-//using Sce.PlayStation.Core.Graphics;
-using Sce.PlayStation.Core.Input;
-
-using Sce.PlayStation.HighLevel.GameEngine2D;
-using Sce.PlayStation.HighLevel.GameEngine2D.Base;
-
-namespace Crystallography
+using Sce.PlayStation.HighLevel.UI; 
+using Sce.PlayStation.HighLevel.GameEngine2D; 
+namespace Pong
 {
 	public class AppMain
 	{
-		//private static GraphicsContext graphics;
+
 		
 		public static void Main (string[] args)
 		{
-			Initialize ();
-
-			while (true) {
-				SystemEvents.CheckEvents ();
-				Update ();
-				Render ();
-			}
-		}
-
-		public static void Initialize ()
-		{
 			Director.Initialize();
-			Game.Instance = new Game();
-			Game.Instance.Initialize();
+            UISystem.Initialize(Director.Instance.GL.Context);
+            Director.Instance.RunWithScene(new TitleScene());               
 			
-			Vector2 ideal_screen_size = new Vector2(960.0f, 544.0f);
-			Camera2D camera = Game.Instance.GameScene.Camera as Camera2D;
-			camera.SetViewFromHeightAndCenter(ideal_screen_size.Y, ideal_screen_size / 2.0f);
-		}
-
-		public static void Update ()
-		{
-			// Query gamepad for current state
-			var gamePadData = GamePad.GetData (0);
-			
-			Director.Instance.Update();
-		}
-
-		public static void Render ()
-		{
-			Director.Instance.Render();
-
-			// Present the screen
-			Sce.PlayStation.HighLevel.GameEngine2D.Director.Instance.GL.Context.SwapBuffers();
-			Director.Instance.PostSwap();
 		}
 	}
 }
+
