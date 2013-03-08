c77f302ca1825e7f2785713d0e9186c64b220384
Ben Johnson <bkbjohnson@gmail.com>
2013-03-07 20:48:28 -0500

    Scoring implemented. Added a UI testbed for broad UI layout stuff.

---
+++ CrystallographyUITestBed/__UICPreview__/ScoreScene.composer.cs
@@ -0,0 +1,138 @@
+﻿// AUTOMATICALLY GENERATED CODE
+
+using System;
+using System.Collections.Generic;
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Imaging;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.HighLevel.UI;
+
+namespace Preview
+{
+    partial class ScoreScene
+    {
+        Label ScoreLabelText;
+        Label ScoreText;
+        Panel Panel_1;
+
+        private void InitializeWidget()
+        {
+            InitializeWidget(LayoutOrientation.Horizontal);
+        }
+
+        private void InitializeWidget(LayoutOrientation orientation)
+        {
+            ScoreLabelText = new Label();
+            ScoreLabelText.Name = "ScoreLabelText";
+            ScoreText = new Label();
+            ScoreText.Name = "ScoreText";
+            Panel_1 = new Panel();
+            Panel_1.Name = "Panel_1";
+
+            // ScoreLabelText
+            ScoreLabelText.TextColor = new UIColor(255f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
+            ScoreLabelText.Font = new UIFont(FontAlias.System, 25, FontStyle.Regular);
+            ScoreLabelText.LineBreak = LineBreak.Character;
+
+            // ScoreText
+            ScoreText.TextColor = new UIColor(255f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
+            ScoreText.Font = new UIFont(FontAlias.System, 18, FontStyle.Regular);
+            ScoreText.LineBreak = LineBreak.Character;
+
+            // Panel_1
+            Panel_1.BackgroundColor = new UIColor(153f / 255f, 153f / 255f, 153f / 255f, 255f / 255f);
+            Panel_1.Clip = true;
+            Panel_1.AddChildLast(ScoreLabelText);
+            Panel_1.AddChildLast(ScoreText);
+
+            // ScoreScene
+            this.RootWidget.AddChildLast(Panel_1);
+
+            SetWidgetLayout(orientation);
+
+            UpdateLanguage();
+        }
+
+        private LayoutOrientation _currentLayoutOrientation;
+        public void SetWidgetLayout(LayoutOrientation orientation)
+        {
+            switch (orientation)
+            {
+                case LayoutOrientation.Vertical:
+                    this.DesignWidth = 544;
+                    this.DesignHeight = 960;
+
+                    ScoreLabelText.SetPosition(0, -12);
+                    ScoreLabelText.SetSize(214, 36);
+                    ScoreLabelText.Anchors = Anchors.None;
+                    ScoreLabelText.Visible = true;
+
+                    ScoreText.SetPosition(0, -12);
+                    ScoreText.SetSize(214, 36);
+                    ScoreText.Anchors = Anchors.None;
+                    ScoreText.Visible = true;
+
+                    Panel_1.SetPosition(0, 444);
+                    Panel_1.SetSize(100, 100);
+                    Panel_1.Anchors = Anchors.None;
+                    Panel_1.Visible = true;
+
+                    break;
+
+                default:
+                    this.DesignWidth = 960;
+                    this.DesignHeight = 544;
+
+                    ScoreLabelText.SetPosition(0, 64);
+                    ScoreLabelText.SetSize(74, 36);
+                    ScoreLabelText.Anchors = Anchors.None;
+                    ScoreLabelText.Visible = true;
+
+                    ScoreText.SetPosition(74, 66);
+                    ScoreText.SetSize(145, 36);
+                    ScoreText.Anchors = Anchors.None;
+                    ScoreText.Visible = true;
+
+                    Panel_1.SetPosition(0, 444);
+                    Panel_1.SetSize(960, 100);
+                    Panel_1.Anchors = Anchors.None;
+                    Panel_1.Visible = true;
+
+                    break;
+            }
+            _currentLayoutOrientation = orientation;
+        }
+
+        public void UpdateLanguage()
+        {
+            ScoreLabelText.Text = "score:";
+
+            ScoreText.Text = "0123456789";
+        }
+
+        private void onShowing(object sender, EventArgs e)
+        {
+            switch (_currentLayoutOrientation)
+            {
+                case LayoutOrientation.Vertical:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void onShown(object sender, EventArgs e)
+        {
+            switch (_currentLayoutOrientation)
+            {
+                case LayoutOrientation.Vertical:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+}
---
+++ CrystallographyUITestBed/__UICPreview__/__DummyProgram.cs
@@ -0,0 +1,113 @@
+// AUTOMATICALLY GENERATED CODE
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Imaging;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.HighLevel.UI;
+
+
+namespace Preview
+{
+    class __DummyProgram
+    {
+        static GraphicsContext graphics;
+
+        static void Main(string[] args)
+        { 
+            graphics = new GraphicsContext(960,544,PixelFormat.Rgba,PixelFormat.Depth16,MultiSampleMode.None);
+            UISystem.Initialize(graphics);
+
+            ScoreScene scene = new ScoreScene();
+            SetupListNum(scene.RootWidget);
+            scene.SetWidgetLayout(LayoutOrientation.Horizontal);
+            UISystem.SetScene(scene);
+            for (; ; )
+            {
+                SystemEvents.CheckEvents();
+
+                // update
+                {
+                    List<TouchData> touchDataList = Touch.GetData(0);
+                    UISystem.Update(touchDataList);
+                }
+
+                // draw
+                {
+                    graphics.SetViewport(0, 0, graphics.Screen.Width, graphics.Screen.Height);
+                    graphics.SetClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+                    graphics.Clear();
+
+                    UISystem.Render();
+                    graphics.SwapBuffers();
+                }
+            }
+        }
+
+        private static void SetupListNum(ContainerWidget container)
+        {
+            foreach (var child in container.Children)
+            {
+                if (child is ListPanel)
+                {
+                    (child as ListPanel).Sections = section;
+                }
+
+                if (child is GridListPanel)
+                {
+                    (child as GridListPanel).ItemCount = 100;
+                    (child as GridListPanel).StartItemRequest();
+                }
+
+                if (child is LiveListPanel)
+                {
+                    (child as LiveListPanel).ItemCount = 100;
+                    (child as LiveListPanel).StartItemRequest();
+                }
+
+                if (child is ContainerWidget)
+                {
+                    SetupListNum(child as ContainerWidget);
+                }
+            }
+        }
+
+        private static ListSectionCollection section = new ListSectionCollection {
+            new ListSection("Section1", 10),
+            new ListSection("Section2", 3),
+            new ListSection("Section3", 30),
+            new ListSection("Section4", 27),
+            new ListSection("Section5", 10),
+            new ListSection("Section6", 20),
+        };
+    }
+
+	public class __UicCustomClassBase__ : Label
+	{
+        public __UicCustomClassBase__()
+        {
+            BackgroundColor = new UIColor(0.3f, 0.3f, 0.3f, 1.0f);
+            VerticalAlignment = VerticalAlignment.Middle;
+            HorizontalAlignment = HorizontalAlignment.Center;
+        }
+        bool update;
+        protected override void OnUpdate(float elapsedTime)
+        {
+            if (!update)
+            {
+                Text = this.ToString();
+                update = true;
+            }
+            base.OnUpdate(elapsedTime);
+        }
+	}
+}
+
+
