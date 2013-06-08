d6230e1f23b366a47f8080b67578eab49aae5304
GameDesignerBen <bkbjohnson@gmail.com>
2013-06-08 00:07:21 -0400

    Level Tool handles new qualities automatically, and ignores Note columns

--- Tools/LevelTool/src/csv2crystallonlevels.py
+++ Tools/LevelTool/src/csv2crystallonlevels.py
@@ -1,6 +1,7 @@
 import csv
 import re
 import os
+import string
 
 #xmlFile = '../../../../data/game/Zombies_GameObjects_Library.xml'
 xmlFilePrefix = './levels/level_'
@@ -12,6 +13,8 @@ csvFile = './crystallonlevels.csv'
 ##pResource = re.compile(r'RESOURCE')
 ##pStore = re.compile(r'STORE')
 ##pMenu = re.compile(r'MENU')
+
+pNote = re.compile(r'NOTE.*',re.IGNORECASE)
 
 
 ##goalData = open(goalFile, 'w')
@@ -32,8 +35,15 @@ rows = []
 csvData = csv.DictReader(open(csvFile, 'rb'))
 for row in csvData:
     # delete the "notes" column
-    del row['']
-    rows.append(row)
+    #del row
+    keys = row.keys()
+    for key in keys:
+        if ( re.match(pNote, key) != None or key == ''):
+            #print key
+            del row[key]
+    if row['Level'] != '':
+        rows.append(row)
+        #print row
 
 levelNum = 0
 numCards = 0
@@ -44,33 +54,41 @@ xmlFile = xmlFilePrefix + str(levelNum) + ".xml"
 xmlData = open(xmlFile, 'w')
 xmlData.write("<Level>\n")
 
-
+## The order is arbitrary, so there is no need to make this a hard-define.
 # the order they need to end up in
-qualities = ['Sound', 'Orientation', 'Color', 'Pattern', 'Particle',
-             'Animation']
+#qualities = ['Sound', 'Orientation', 'Color', 'Pattern', 'Particle',
+#             'Animation']
+
+levelCounter = 0
 
 for row in rows:
     if ( row['Level'] != str(levelNum) ): # end current level description and start another
         xmlData.write("</Level>")
+        levelCounter += 1
         xmlData.close()
         levelNum = int(row['Level'])
         xmlFile = xmlFilePrefix + str(levelNum) + ".xml"
         xmlData = open(xmlFile, 'w')
         xmlData.write("<Level>\n")
     xmlData.write("\t<Card>\n")
-    for quality in qualities:
-        if quality == 'Particle' and row[quality] == '':
-            row[quality] = "NONE 0"
-        if row[quality] == '':
-            continue
-        numQualities += 1
-        xmlData.write('\t\t<Quality Name="Q' + quality + '" Value="' + str(row[quality][-1]) + '" />\n')
+    for entry in row: # add qualities to a card
+        if ( entry != 'Level' and row[entry] != '' ):
+            numQualities += 1
+            xmlData.write('\t\t<Quality Name="Q' + entry + '" Value="' + str(row[entry][-1]) +'" />\n')
+#    for quality in qualities:
+#        if quality == 'Particle' and row[quality] == '':
+#            row[quality] = "NONE 0"
+#        if row[quality] == '':
+#            continue
+#		numQualities += 1
+#        xmlData.write('\t\t<Quality Name="Q' + quality + '" Value="' + str(row[quality][-1]) + '" />\n')
     xmlData.write("\t</Card>\n")
     numCards += 1
 
 xmlData.write("</Level>") # finish the very last level description
+levelCounter += 1
 xmlData.close()
 
-print "Level files generated: " + str(levelNum + 1)
+print "Level files generated: " + str(levelCounter + 1)
 print "Cards generated: " + str(numCards)
 print "Qualities generated: " + str(numQualities)
