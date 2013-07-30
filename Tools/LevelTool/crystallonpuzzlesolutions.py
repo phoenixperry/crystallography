9184ddd6c7af9b47eedd568a939488b62159398e
GameDesignerBen <bkbjohnson@gmail.com>
2013-07-30 17:28:43 -0400

    Puzzle Solution script updated
    crystallonpuzzlesolutions.py now takes the maximum screen population
    into account when determining all possible playthrough solutions.

--- Tools/LevelTool/crystallonpuzzlesolutions.py
+++ Tools/LevelTool/crystallonpuzzlesolutions.py
@@ -8,165 +8,160 @@ csvFile = './crystallonlevels.csv'
 
 pNote = re.compile(r'NOTE.*',re.IGNORECASE)
 
-
-#p1 = re.compile(r'(.+)(^\t<group name="Goals".*?\t</group>)(.+</objects>)',re.MULTILINE+re.DOTALL)
-
-##m = re.search(p1, xmlDataIn.read())
-
-##m = xmlDataIn.read()
-
-##xmlData.write(m.group(1))
-
 if not os.path.exists(outFilePrefix):
     os.makedirs(outFilePrefix)
 
 rows = []
 csvData = csv.DictReader(open(csvFile, 'rb'))
 for row in csvData:
-    # delete the "notes" column
-    #del row
     keys = row.keys()
     for key in keys:
         if ( re.match(pNote, key) != None or key == ''):
-            #print key
             del row[key]
     if row['Level'] != '':
         rows.append(row)
-        #print row
 
 levelNum = 0
+maxPop = 18
 numCards = 0
 numQualities = 0
+allPossibleCubes = []
+allPossibleCubeScores = []
+deck = {}
+cardNumbers = []
+scoringQualities = {}
+cubeChain = {}
+scoreChain = {}
+
 
 #prepare the very first level description
 outFile = outFilePrefix + "crystallon_analysis.csv"
 outData = open(outFile, 'w')
 outData.write("Level,Length,Points,Contents,Lengths,Scores\n")
 
-cubeChain = {}
-
 def main():
     global levelNum
     rowCounter = 1
     levelCards = {}
-    scoreChain = {}
+    global scoreChain
+    global cubeChain
+    global deck
     
     for row in rows:
         rowCounter += 1
         qualities = {}
         
         if ( row['Level'] != str(levelNum) ): # end of current level's data reached
-            possible, scores = findAllPossibleCubes( levelCards )
-            if int(levelNum) == 3:
-                print possible
-            findRemainingCubes(possible)
-            # SORT CHAINS FROM LONGEST TO SHORTEST
+            print levelNum
+            deck = levelCards
+
+            # INITIAL LEVEL SETUP
+            SetUpLevel( deck ) #this populates deck and cardNumbers
+
+            # FIND ALL POSSIBLE PLAYTHROUGHS
+            if( len(cardNumbers) > 18 ):
+                SimulatePlay( cardNumbers[:17], cardNumbers[18:] )
+            else:
+                SimulatePlay( cardNumbers, [] )
+
+
+            # REMOVE NON-UNIQUE PLAYTHROUGHS
             sortedKeys = cubeChain.keys()
             sortedKeys.sort()
-            chainsWithoutSubsets = cubeChain.copy() # working copy, so we don't mutate the thing we're iterating over
-            if int(levelNum) == 3:
-                print '--------------------'
-                print sortedKeys
-#                for key in sortedKeys:
-#                    print str(key) + ": " + str(cubeChain[key])
-#                    print ""
+            
             for key in sortedKeys:
                 if key == sortedKeys[-1]:
                     break
-                if int(levelNum) == 3:
-                    print str(key+1) + ", " + str(key)
-                cubeChain[key] = removeSubsets( cubeChain[key+1], cubeChain[key] )
-                chainsWithoutSubsets[key] = cubeChain[key]
-#                for chain1 in cubeChain[key+1]:
-#                    for chain2 in cubeChain[key]:
-#                        unique = False
-#                        for cube in chain2:
-#                            # if a short chain contains a cube that is not found in a long chain, the short chain is not a subset
-#                            if chain1.count(cube) == 0:
-#                                unique = True
-#                                if int(levelNum) == 3:
-#                                    print str(chain1) + "\n" + str(chain2) + ": " + str(cube) + " " + str(chain1.count(cube)) + "\n"
-#                                break
-#                            else:
-#                                if int(levelNum) == 3:
-#                                    print str(chain1) + "\n" + str(chain2) + ": " + str(cube) + " " + str(chain1.count(cube)) + "\n"
-#                        # if a short chain is found to be a subset, remove it
-#                        if unique == False and chainsWithoutSubsets[key].count(chain2) > 0:
-#                            chainsWithoutSubsets[key].remove(chain2)
-            for key in chainsWithoutSubsets.keys():
-                for chain in chainsWithoutSubsets[key]:
-                    score = 0
-                    for cube in chain:
-                        score += scores[possible.index(cube)]
-                    if not scoreChain.has_key(score):
-                        scoreChain[score] = []
-                    scoreChain[score].append(chain)
+                cubeChain[key] = RemoveSubsets( cubeChain[key+1], cubeChain[key] )
+
+            # CALCULATE TOTAL SCORES FOR CUBE UNIQUE CHAINS
+            BuildScoreChains( cubeChain ) # this populates scoreChain
+
+            # OUTPUT HEADER ROW FOR LEVEL
             possibleLengths = ''
             possibleScores = ''
             for key in sortedKeys:
-                if len(chainsWithoutSubsets[key]) > 0:
-                    possibleLengths += str(key) + '(' + str(len(chainsWithoutSubsets[key])) + ') '
+                if len(cubeChain[key]) > 0:
+                    possibleLengths += str(key) + '(' + str(len(cubeChain[key])) + ') '
             sortedScores = scoreChain.keys()
             sortedScores.sort()
             for score in sortedScores:
                 possibleScores += str(score) + '(' + str(len(scoreChain[score])) + ') '
             outData.write( str(levelNum) + ',' + 'XX,' + 'XX,' + 'XXXXXXX,' + possibleLengths + ',' + possibleScores + '\n')
+
+            # OUTPUT EACH POSSIBLE CUBE CHAIN, IT'S LENGTH, AND SCORE
             for key in sortedKeys:
-#                if ( int(levelNum) == 5):
-#                    print str(key) + ': ' + str(len(chainsWithoutSubsets[key]))
-                for chain in chainsWithoutSubsets[key]:
-#                    if ( int(levelNum) == 5):
-#                        print str(chain)
+                for chain in cubeChain[key]:
                     score = 0
                     contents = ''
                     for cube in chain:
-                        score += scores[possible.index(cube)]
+                        score += allPossibleCubeScores[allPossibleCubes.index(cube)]
                         contents += '['
                         for piece in cube:
-                            contents += piece + ' '
+                            contents += str(piece) + ' '
                         contents += '] '
                     outData.write( str(levelNum) + ',' + str(key) + ',' + str(score) + ',' + contents + '\n')
-                    if int(levelNum) == 3:
-                        print str(key) + ", " + str(score) + ": " + contents
+
+            # CLEAN UP
             levelCards.clear()
             cubeChain.clear()
             scoreChain.clear()
             levelNum = row['Level']
 
+        # GATHER CARD DATA
         for entry in row: # add qualities to a card
             qualities.update({entry: str(row[entry])})
         levelCards.update({str(rowCounter): qualities})
-        #print levelCards
+        
 
-def removeSubsets ( longChains, shortChains ):
+#################################################################################
+# RemoveSubsets()
+#
+# Removes cube chains from shortChains that are subsets of a chain in longChains
+#################################################################################
+def RemoveSubsets ( longChains, shortChains ):
     for longChain in longChains:
         for shortChain in shortChains:
             if shortChain == None:
                 continue
             match = 0
             for cube in shortChain:
-                if longChain.count(cube) > 0:
+                try:
+                    longChain.index(cube)
+                except ValueError:
+                    continue
+                else:
                     match += 1
+            # IF EVERY CUBE IN THIS SHORT CHAIN IS CONTAINED IN ANY LONGER CHAIN, IT IS A SUBSET, SO MARK IT FOR REMOVAL
             if len(shortChain) == match:
                 shortChains[shortChains.index(shortChain)] = None
+    # REMOVE MARKED CHAINS FROM shortChains
     while shortChains.count(None):
         shortChains.remove(None)
     return shortChains
-            
-    
 
-def findAllPossibleCubes( pDict ):
-    global levelNum
-    cubes = []
-    scores = []
-    scoringQualities = {}
-    deck = pDict.values()
+
+#######################################################################################
+# SetUpLevel()
+#
+# Establishes level start conditions
+#######################################################################################
+def SetUpLevel( pDict ):
+#    global deck
+#    deck = pDict.values()
+    global cardNumbers
     cardNumbers = pDict.keys()
-    if (len(deck) < 3):
-        return cubes, scores
-    
-    # Build list of scoring qualities
-    for card in deck:
+    cardNumbers.sort()
+    global scoringQualities
+    scoringQualities = {}
+    global allPossibleCubes
+    allPossibleCubes = []
+    global allPossibleCubeScores
+    allPossibleCubeScores = []
+
+    # BUILD DICTIONARY OF SCORING QUALITIES FOR THIS LEVEL
+    for cardID in cardNumbers:
+        card = deck[cardID]
         for quality in card.keys():
             if quality == 'Orientation':
                 continue
@@ -174,33 +169,102 @@ def findAllPossibleCubes( pDict ):
                 scoringQualities[quality] = []
             if scoringQualities[quality].count(card[quality]) == 0:
                 scoringQualities[quality].append(card[quality])
+
+    # FIND ALL POSSIBLE CUBES AND THEIR POINT VALUES
+    allPossibleCubes, allPossibleCubeScores = FindPossibleCubes( cardNumbers, True )
+
+
+#######################################################################################
+# SimulatePlay()
+#
+# Finds all possible valid outcomes of a given deck state
+#######################################################################################
+def SimulatePlay ( pOnScreen, pLeftover, pPreviousCubes=[] ):
+    global deck
+    global cardNumbers
+    global scoringQualities
+
+    # Find all possible cubes that can be made with the cards on screen
+    Cubes = FindPossibleCubes( pOnScreen, False )
+    
+    # If no possible cubes can be made from this on-screen card population, return
+    counter = len(Cubes)
+    if( counter == 0 ):
+        if cubeChain.has_key(len(Cubes)):
+            cubeChain[counter].append(pPreviousCubes[:])
+        else:
+            cubeChain[counter] = [pPreviousCubes[:]]
+        return
+    
+    # Recursively create all possible playthroughs
+    if ( len(pLeftover) > 0 ):
+        newLeftover = []
+        adds = []
+
+        # draw the next 3 cards and add them to the screen
+        if ( len(pLeftover) > 3):
+            newLeftover = pLeftover[3:]
+            adds = pLeftover[:2]
+        else:
+            adds = pLeftover[:]
+
+        # Recursion: Simulate Play based on every cube the player could possibly make with this screen population
+        for cube in Cubes:
+            newOnScreen = pOnScreen[:]
+            newCubes = pPreviousCubes[:]
+            newCubes.append(cube)
+            
+            for card in cube:
+                try:
+                    newOnScreen.remove(str(card))
+                except ValueError:
+                    print newOnScreen, card
+            newOnScreen.extend(adds)
+            SimulatePlay( newOnScreen, newLeftover, newCubes  )
+            
+    # If there are no cards left in reserve, simulate all possible remaining playthroughs from this state
+    else:
+        BuildCubeChains( Cubes, pPreviousCubes, len(pPreviousCubes) )
+
+
+##########################################################################################
+# FindPossibleCubes()
+#
+# Returns a list of all possible valid cubes given a particular card population on screen
+##########################################################################################
+def FindPossibleCubes( pCardsOnScreen, pReturnScores=False ):
+    global levelNum
+    global maxPop
+    global scoringQualities
+    global deck
+    global cardNumbers
+    cubes = []
+    scores = []
+
+    if (len(pCardsOnScreen) < 3):
+        return cubes, scores
                 
     # Build a list of all possible cubes, valid or not, filtering only orientation
-    for i in range(0, len(deck)-2):
-        card1 = deck[i]
-        for j in range(i+1, len(deck)-1):
-            card2 = deck[j]
-            for k in range(j+1, len(deck)):
-                score = 0
-                card3 = deck[k]
+    for i in range(0, len(pCardsOnScreen)-2):
+        card1 = deck[pCardsOnScreen[i]]
+        for j in range(1, len(pCardsOnScreen)-1):
+            card2 = deck[pCardsOnScreen[j]]
+            for k in range(2, len(pCardsOnScreen)):
+                card3 = deck[pCardsOnScreen[k]]
                 if card1['Orientation'] == card2['Orientation'] or card1['Orientation'] == card3['Orientation'] or card2['Orientation'] == card3['Orientation']:
                     continue
-                cube = [cardNumbers[i], cardNumbers[j], cardNumbers[k]]
+                cube = [int(pCardsOnScreen[i]), int(pCardsOnScreen[j]), int(pCardsOnScreen[k])]
+                # Put the sides in numerical order to make the output easier to read
                 cube.sort()
                 cubes.append(cube)
 
+    # Put the cubes in numerical order to make the output easier to read
     cubes.sort()
-#    if int(levelNum) == 5:
-#        print cubes
-#    sortedCubes = cubes[:]
 
     # Remove invalid cubes and determine the point value of all the valid cubes
     for cube in cubes:
-        score = calculateScore( [deck[cardNumbers.index(cube[0])], deck[cardNumbers.index(cube[1])], deck[cardNumbers.index(cube[2])]], scoringQualities )
+        score = CalculateScore( [deck[str(cube[0])], deck[str(cube[1])], deck[str(cube[2])]], scoringQualities )
         if score == -1:
-#            if int(levelNum) == 5:
-#                print "Illegal Cube"
-#                print "----"
             cubes[cubes.index(cube)] = None
         else:
             if score == 0:
@@ -208,11 +272,19 @@ def findAllPossibleCubes( pDict ):
             scores.append(score)
     while cubes.count(None) > 0:
         cubes.remove(None)
-        
-    return cubes, scores
 
-def calculateScore ( pCube, pScoringQualities ):
-    global levelNum
+    if pReturnScores:
+        return cubes, scores
+    else:
+        return cubes
+
+                
+##############################################################################
+# CalculateScore
+#
+# Determines the point value of a given cube in a given level
+##############################################################################
+def CalculateScore ( pCube, pScoringQualities ):
     score = 0
     for quality in pCube[0].keys():
         isSame = False
@@ -225,17 +297,18 @@ def calculateScore ( pCube, pScoringQualities ):
             isDifferent = True
             if quality != 'Orientation' and len(pScoringQualities[quality]) ==3:
                 score += 3
-#        if int(levelNum) == 5:
-#            print str(quality) + ": " + str(isSame) + " " + str(isDifferent)
         if not (isSame or isDifferent):
             score = -1
             break
-#    if int(levelNum) == 5:
-#        print "-----------------------------"
     return score
-        
 
-def findRemainingCubes( pFreshList, pUsedList=[[]], pCounter=0 ):
+        
+######################################################################################
+# BuildCubeChains()
+# 
+# Finds any possible remaining cubes when there are no more cards left in reserve
+######################################################################################
+def BuildCubeChains( pFreshList, pUsedList=[], pCounter=0 ):
     global cubeChain
     global levelNum
     newCounter = pCounter # this is the "depth" of the recursion, i.e. how many cubes we've made already
@@ -264,29 +337,40 @@ def findRemainingCubes( pFreshList, pUsedList=[[]], pCounter=0 ):
                 newFreshList.remove(None)
                         
             # if there are more possible cubes, explore deeper
-#            if (len(newFreshList) > 0):
-            if True:
-                isTerminus = pCounter == findRemainingCubes( newFreshList, newUsedList, newCounter )
+            isTerminus = pCounter == BuildCubeChains( newFreshList, newUsedList, newCounter )
                 
-#    else:
-    newUsedList.sort()
-    if cubeChain.has_key(pCounter):
-        cubeChain[pCounter].append(pUsedList[1:])
+    pUsedList.sort()
+    length = len(pUsedList)
+    if cubeChain.has_key(length):
+        try:
+            cubeChain[length].index(pUsedList)
+        except ValueError:
+#        if cubeChain[key].count(pUsedList) == 0:
+            cubeChain[length].append(pUsedList[:])
     else:
-        cubeChain[pCounter] = [pUsedList[1:]]
-#    if int(levelNum) == 5:
-#        print str(pCounter) + ': ' + str(pUsedList[1:])
-
-    
-#    if pCounter == 0:
-#        if int(levelNum) == 5:
-#            for key in cubeChain:
-#                print str(key) + ": "
-#                for cube in cubeChain[key]:
-#                    print str(cube)
-#                print ""
+        cubeChain[length] = [pUsedList[:]]
     
     return newCounter
+
+
+################################################################################
+# BuildScoreChains()
+#
+# Calculate total scores for each cube chain
+################################################################################
+def BuildScoreChains( pChains ):
+    global scoreChain
+    global allPossibleCubeScores
+    global allPossibleCubes
+
+    for key in pChains.keys():
+        for chain in pChains[key]:
+            score = 0
+            for cube in chain:
+                score += allPossibleCubeScores[allPossibleCubes.index(cube)]
+            if not scoreChain.has_key(score):
+                scoreChain[score] = []
+            scoreChain[score].append(chain)
 
 
 if __name__ == '__main__':
